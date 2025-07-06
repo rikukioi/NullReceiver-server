10bbfbd3038dbdd73b0ecea9fecda4abe0c78296
rikukioi <alexandr.petrenko.24@mail.ru>
2025-07-06 19:06:41 +0500

    Добавлена обертка для FastAPI Depends

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -1,5 +1,5 @@
 from .session import get_session
-from .uow import get_auto_session, UnitOfWork
+from .uow import get_auto_session, get_uow_dep, UnitOfWork
 from . import models
 
-__all__ = ["get_session", "models", "get_auto_session", "UnitOfWork"]
+__all__ = ["get_session", "models", "get_auto_session", "get_uow_dep", "UnitOfWork"]
--- src/database/uow.py
+++ src/database/uow.py
@@ -2,24 +2,33 @@ from contextlib import asynccontextmanager
 from typing import AsyncGenerator
 from sqlalchemy.ext.asyncio import AsyncSession
 from .session import AsyncSessionLocal
+from src.database.repository import UserRepository
 
 
 class UnitOfWork:
     def __init__(self, session: AsyncSession):
         self.session = session
+        self.user_repo = UserRepository(self.session)
 
     async def __aenter__(self):
         return self
 
     async def __aexit__(self, exc_type, exc, tb):
-        if exc_type:
-            await self.session.rollback()
-        else:
-            await self.session.commit()
+        try:
+            if exc_type:
+                await self.session.rollback()
+            else:
+                await self.session.commit()
+        finally:
+            self.session.close()
 
 
 @asynccontextmanager
 async def get_auto_session() -> AsyncGenerator[UnitOfWork, None]:
-    async with AsyncSessionLocal() as session:
-        async with UnitOfWork(session) as uow:
-            yield uow
+    async with UnitOfWork(AsyncSessionLocal()) as uow:
+        yield uow
+
+
+async def get_uow_dep() -> UnitOfWork:
+    async with get_auto_session() as uow:
+        return uow
