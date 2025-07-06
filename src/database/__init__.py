1a5e4f81774986e1850fe64b9c327b777c90c268
rikukioi <alexandr.petrenko.24@mail.ru>
2025-07-06 17:47:38 +0500

    Добавил UnitOfWork в init для подсказок типов

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -1,5 +1,5 @@
 from .session import get_session
-from .uow import get_auto_session
+from .uow import get_auto_session, UnitOfWork
 from . import models
 
-__all__ = ["get_session", "models", "get_auto_session"]
+__all__ = ["get_session", "models", "get_auto_session", "UnitOfWork"]
