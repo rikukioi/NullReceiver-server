68baa86c64c01bb8ad984564c49dc7484008ff41
rikukioi <alexandr.petrenko.24@mail.ru>
2025-07-06 20:44:40 +0500

    Поправил alembic на использование синхронного движка при миграциях

--- alembic/env.py
+++ alembic/env.py
@@ -6,8 +6,7 @@ from alembic import context
 from src.core.config import settings
 from src.database.models.base import Base
 
-DATABASE_URL = settings.postgres.database_url
-
+DATABASE_URL = settings.postgres.database_url.replace("+asyncpg", "")
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
